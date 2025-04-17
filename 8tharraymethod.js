9ed0289354add57897211a811cbc856ee5496bc1
tanimhasann <hasantanim918@gmail.com>
2025-04-17 12:00:31 +0600

    4th day

---
+++ 8tharraymethod.js
@@ -0,0 +1,10 @@
+let number=['Tanim','rafi','skaib'];
+
+number.push('lion')//add the new value in the last last element
+console.log(number);
+number.shift()//its remove the first element of the array
+console.log(number);
+number.unshift('hasib');//its add the new value at 1st position
+console.log(number);
+number.pop();//pop delete the last element of the array
+console.log(number);
