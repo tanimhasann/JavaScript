335cc3ccc5f5d377b549048cfea841aa5000e254
tanimhasann <hasantanim918@gmail.com>
2025-04-22 10:46:40 +0600

    day5

---
+++ 10thswitch.js
@@ -0,0 +1,13 @@
+let person='sakib';
+switch(person)
+{
+    case 'Tanim':
+        console.log("no one batsman in bangladesh");
+        break;
+    case 'sakib':
+        console.log('No one alrounder');
+        break;
+    case 'musfiq':
+       console.log("wicket kepper");
+       break;
+}
---
+++ 11switch.js
@@ -0,0 +1,25 @@
+switch(new Date().getDay())//this function is using for findout the day
+{
+        case 0:
+        console.log("sunday");
+        break;
+        case 1:
+        console.log("monday");
+         break;
+           case 2:
+        console.log("Tuesday");
+         break;
+           case 3:
+        console.log("Wednesday");
+         break;
+           case 4:
+        console.log("Thursday");
+         break;
+           case 5:
+        console.log("Friday");
+         break;
+           case 6:
+        console.log("Saturday");
+           
+        
+}
